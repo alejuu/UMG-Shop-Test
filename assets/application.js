c16577e980abca68ac40c71b24d673c0adf753c0
alejuu <contact@alejuu.com>
2023-10-23 23:58:46 -0500

    Add Ajax Cart

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,12 @@
 // Put your application javascript here
+function runWhenInit() {
+  console.log("The current cart state is: ", window.liquidAjaxCart.cart);
+}
+
+if (window.liquidAjaxCart?.init) {
+  // if Liquid Ajax Cart is already initialized
+  runWhenInit();
+} else {
+  // wait for Liquid Ajax Cart to be initialized
+  document.addEventListener("liquid-ajax-cart:init", runWhenInit);
+}
